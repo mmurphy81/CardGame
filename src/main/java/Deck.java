ae92dcdf3a58ec2127a854a337e63615646bceff
meramurphy <mera.murphy@menloschool.org>
2025-11-19 13:56:35 -0800

    fixed shuffle and deck errors

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -6,14 +6,13 @@ public class Deck {
 
     public Deck(String[] rank, String[] suit, int[] values) {
         cards = new ArrayList<Card>();
-        cardsLeft = cards.size();
         for (int i=0; i<rank.length; i++){
             for (int j = 0; j<suit.length; j++){
                 Card x = new Card(rank[i], suit[j], values[i]);
-                cards.set(i, x);
-                cardsLeft ++;
+                cards.add(x);
             }
         }
+        cardsLeft = cards.size();
     }
 
     public boolean isEmpty(){
@@ -38,9 +37,11 @@ public class Deck {
     }
 
     public void shuffle(){
-        for (int i = cardsLeft; i>0; i--){
-            int r = (int)(Math.random()*(getCardsLeft()-1)+1);
+        for (int i = cardsLeft - 1; i>=0; i--){
+            int r = (int)(Math.random()*(i));
+            Card temp = cards.get(i);
             cards.set(i, cards.get(r));
+            cards.set(r, temp);
         }
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 public class Game {
     static void main(String[] args) {
-
+        Deck d = new Deck(new String[]{"A", "2", "3"}, new String[]{"Hearts", "Clubs"}, new int []{1,2,3});
+        d.shuffle();
+        System.out.println();
     }
 }
