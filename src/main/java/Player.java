aa36760147a277d829b4bbd956738ce28bfffc4b
meramurphy <mera.murphy@menloschool.org>
2025-11-21 09:06:56 -0800

    Finished player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class Player {
+    private int points;
+    private ArrayList<Card> hand;
+    private String name;
+
+    public Player(String name){
+       this.name = name;
+       points=0;
+    }
+    public Player(String name, ArrayList<Card> hand){
+        points=0;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPoints(int numPoints){
+        points+=numPoints;
+    }
+    public void addCard(Card newCard){
+        hand.add(newCard);
+    }
+
+    public String toString(){
+        return name + " has " + points + " points" +"/n" + name + "'s cards:" + hand;
+    }
 }
