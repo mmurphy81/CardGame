da46a5debc2f8a9a8f32c5191f41a020271cc986
meramurphy <mera.murphy@menloschool.org>
2025-12-03 11:43:52 -0800

    Finished my game functions

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,10 +10,12 @@ public class Game {
         this.player2 = new Player("Player2");
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"};
         String[] suits = {"Hearts", "Clubs", "Spades", "Diamonds"};
-        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
         this.deck = new Deck(ranks, suits, values);
 
         while (!deck.isEmpty()) {
+            deck.shuffle();
             Card player1Cards = deck.deal();
             player1.addCard(player1Cards);
 
@@ -22,26 +24,68 @@ public class Game {
         }
     }
 
-    public String printInstructions(){
-        return "Welcome to Egyption rat slap!" + "/n" + "Here are some rules: " + "/n" +
-                "The goal of the game is to end up with the most cards. You can win cards by placing face cards" +
-                " and your opponent will need to place the said amount of cards corresponding to whatever face card you placed." +
-                " If you place a Jack, your opponent needs to place one card. If that card is a face card too, you will need" +
-                " to place a certain amount of cards as well. Jack = 1 card, Queen = 2 cards, King = 3 cards, Ace = 4 cards.";
-    }
-    public boolean wonDeck(){
+    public void playGame(){
+        ArrayList<Card> pile = new ArrayList<>();
+        while (player1.getHand().size() > 0 && player2.getHand().size() > 0) {
+            System.out.println("Player1 hand size: "+  player1.getHand().size() + " Player 2 hand size: "+  player2.getHand().size());
+            if (hasWon()==null){
+                Card player1Card = player1.placeCard();
+                System.out.print(player1Card + " ");
+                pile.add(player1Card);
 
-    }
+                if (player1Card.faceCard()==true){
+                    for (int i =0; i<pile.size(); i++){
+                        player1.addCard(pile.get(i));
+                        pile.remove(i);
+                    }
+                    System.out.println("Player 1 has won this pile.");
+                }
 
-    public void playCard(Card card){
-        while (wonDeck!= true){
-            deck+=player1Deck.deal();
-            deck+=player2Deck.deal();
+                Card player2Card = player2.placeCard();
+                System.out.print(player2Card + " ");
+                pile.add(player2Card);
+                if (player2Card.faceCard()==true){
+                    for (int i =0; i<pile.size(); i++){
+                        player2.addCard(pile.get(i));
+                        pile.remove(i);
+                    }
+
+                    System.out.println("Player 2 has won this pile.");
+                }
+            }
+
+            else if (hasWon().equals(player1)){
+                System.out.println("Player 1 has won!");
+                break;
+            }
+            else if (hasWon().equals(player2)){
+                System.out.println("Player 2 has won!");
+                break;
+            }
+
         }
-
     }
 
-    static void main(String[] args) {
+    public Player hasWon(){
+        if (player1.getHand().size()<2){
+            return player2;
+        }
+        else if (player2.getHand().size()<2){
+            return player1;
+        }
+        return null;
+    }
 
+    public static void printInstructions(){
+        System.out.println("Welcome to Egyption rat slap!\n" + "Here are some rules: \n" +
+                "The goal of the game is to end up with the most cards. You can win cards by placing face cards" +
+                "When someone places a facecard, they win the deck.");
+    }
+
+
+    public static void main(String[] args) {
+        printInstructions();
+        Game game1 = new Game();
+        game1.playGame();
     }
 }
